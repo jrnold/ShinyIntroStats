a273a5f7b2f23f74ff4a7955b052348ab931b3a0
jrnold <jeffrey.arnold@gmail.com>
2015-03-03 03:41:00 -0800

    bugfix: wrong mean / sd

--- inst/shiny/normal_distribution/server.R
+++ inst/shiny/normal_distribution/server.R
@@ -4,7 +4,7 @@ shinyServer(function(input, output) {
   output$plot <-
       renderPlot({
           n <- 1000
-          x <- seq(input$mean1 - 6 * input$sd2,
+          x <- seq(input$mean2 - 6 * input$sd2,
                    input$mean2 + 6 * input$sd2,
                    length.out = n)
           y1 <- dnorm(x, input$mean1, input$sd1)
@@ -12,7 +12,7 @@ shinyServer(function(input, output) {
           dd <- data.frame(x = rep(x, 2),
                            y = c(y1, y2),
                            distribution = factor(rep(c(1L, 2L), each = n)))
-          breaks <- input$mean1 + -6:6 * input$sd2
+          breaks <- input$mean2 + -6:6 * input$sd2
           (ggplot(dd, aes(x = x, ymax = y, ymin = 0, fill = distribution))
            + geom_ribbon(alpha = 0.5)
            + scale_y_continuous("p(x)")
